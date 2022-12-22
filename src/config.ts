0a62205e8788d81c5f95b4074c8020edb0fd6aa9
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2022-12-22 19:02:39 -0300

    [FEATURE] Create IconOrchester and types

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,23 @@
+import type { IconEnum } from 'types'
+
 export const SITE_TITLE = 'My personal website.'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
+
+export const TECHNOLOGIES_ICONS_LIST: Array<IconEnum> = [
+	'javascript',
+	'typescript',
+	'react',
+	'html',
+	'css',
+	'vitejs',
+	'figma',
+	'astro',
+	'sass',
+	'deno',
+	'nodejs',
+	'nextjs',
+	'git',
+	'preact',
+	'redux',
+	'jest',
+]

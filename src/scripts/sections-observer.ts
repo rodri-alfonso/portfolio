7982e2f25a9f3986f0fe4cfef35709c4f4bd3e84
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-01-18 12:26:22 -0300

    [FIX]: Fix sections-observer script

--- src/scripts/sections-observer.ts
+++ src/scripts/sections-observer.ts
@@ -1,33 +1,41 @@
 import { debounce } from 'utils'
 
+const DEFAULT_DEBOUNCE_TIME = 100
+const SECTION_QUERY_INDEX = 2
+
 const sectionsRef = Array.from(document.querySelectorAll('[section-query]')) as Array<HTMLElement>
 
 let prevAnchorRef: HTMLAnchorElement | null
-let prevId: string = ''
+let prevHash: string = ''
 
-function handleObserver(entries: any) {
+function handleObserver(entries: Array<IntersectionObserverEntry>) {
 	debounce(() => {
-		entries.forEach((entry: any) => {
-			let id = entry.target.attributes['section-query'].value
+		entries.forEach((entry: IntersectionObserverEntry) => {
+			let hash: string = entry.target.attributes[SECTION_QUERY_INDEX].value
 
-			if (entry.intersectionRatio < 0.21) return
-			if (prevId === id) return
+			if (entry.intersectionRatio === 0) {
+				prevAnchorRef && prevAnchorRef.classList.remove('active')
+				window.history.replaceState(null, '', '/')
+				return
+			}
+			if (prevHash === hash) return
 
-			const anchorRef = document.getElementById(`link-${id}`) as HTMLAnchorElement
-			window.history.replaceState(null, '', '#' + id)
+			const anchorRef = document.getElementById(`link-${hash}`) as HTMLAnchorElement
+
+			window.history.replaceState(null, '', '#' + hash)
 
 			if (prevAnchorRef) prevAnchorRef.classList.remove('active')
 			anchorRef.classList.add('active')
 
 			prevAnchorRef = anchorRef
-			prevId = id
+			prevHash = hash
 		})
-	}, 160)
+	}, DEFAULT_DEBOUNCE_TIME)
 }
 
 const observer = new IntersectionObserver(handleObserver, {
-	threshold: Array.from({ length: 80 }, (_, x) => x / 80),
-	rootMargin: `0px 0px -60px 0px`,
+	threshold: Array.from({ length: 100 }, (_, x) => x / 100),
+	rootMargin: '-200px 0px -470px 0px',
 })
 
 for (let section of sectionsRef) {

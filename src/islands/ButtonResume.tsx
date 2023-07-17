c8321ad5a7c92086c07e473014f98285049a8362
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-07-17 18:21:03 -0300

    [UPDATE]: Add lang detection to cv view

---
+++ src/islands/ButtonResume.tsx
@@ -0,0 +1,13 @@
+export default function ButttonResume() {
+	const NAVIGATOR_LANG = window.navigator.language.includes('es') ? 'ES' : 'EN'
+
+	return (
+		<a
+			target='_blank'
+			href={`/${NAVIGATOR_LANG}_CV_RODRIGO_ALFONSO.pdf`}
+			class='px-3 py-2 rounded-lg uppercase bg-gray-800 hover:bg-white hover:text-black border-2 border-gray-800 text-white font-semibold transition-all active:scale-95'
+		>
+			<p>check resume</p>
+		</a>
+	)
+}

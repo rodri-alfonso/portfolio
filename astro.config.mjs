1b82ba05a04fe7e7b73b7f8718aa74c24e9c4fec
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-07-20 00:44:03 -0300

    [UPDATE] Update vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import deno from '@astrojs/deno'
-import compress from 'astro-compress'
-import tailwind from '@astrojs/tailwind'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import deno from '@astrojs/deno';
+import compress from 'astro-compress';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [tailwind(), sitemap(), compress(), preact()],
-	output: 'server',
-	adapter: deno(),
-})
+  site: 'https://example.com',
+  integrations: [tailwind(), sitemap(), compress(), preact()],
+  output: 'server',
+  adapter: vercel()
+});

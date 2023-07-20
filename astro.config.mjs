e37b4e3c75a35a76cc5fbc1263dae2f0b0080c8d
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-07-20 00:50:46 -0300

    [UPDATE] Update type of deploy to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import deno from '@astrojs/deno'
 import compress from 'astro-compress'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [tailwind(), sitemap(), compress(), preact()],
-	output: 'static',
+	output: 'server',
 	adapter: vercel(),
 })
